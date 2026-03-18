42933914c0b08f9f661fa254f5af9504134defcf
iam-vasanth <vk.vasanth.r@gmail.com>
2026-03-18 13:29:12 +0000

    WIP

--- modules/profiles/desktop/home/symlinks.nix
+++ modules/profiles/desktop/home/symlinks.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 {
+  home.file."Pictures/Wallpapers".source = ../../../../Wallpapers;
   home.file.".config/fastfetch/ascii.txt".source = ./ascii.txt;
   home.file.".ssh/github_ssh.pub".source = ../../../../.secrets/public_keys/github_ssh.pub;
 }
