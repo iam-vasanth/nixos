9bde2035e051355ce0cd8747ec12707f9a361c05
iam-vasanth <vk.vasanth.r@gmail.com>
2026-03-24 06:40:11 +0530

    WIP

--- modules/profiles/server/nixos/homepage.nix
+++ modules/profiles/server/nixos/homepage.nix
@@ -3,9 +3,55 @@
 {
   services.glance = {
     enable = true;
-    settings = ../configs/glance.yml;
+    settings = {
+      server = {
+        host = "0.0.0.0";
+        port = 8080;
+      };
+      pages = [
+        {
+          name = "Home";
+          columns = [
+            {
+              size = "small";
+              widgets = [
+                { type = "clock"; hour-format = "24h"; }
+                { type = "calendar"; }
+              ];
+            }
+            {
+              size = "full";
+              widgets = [
+                {
+                  type = "bookmarks";
+                  groups = [
+                    {
+                      title = "Services";
+                      links = [
+                        { title = "Immich"; url = "http://192.168.122.130:2283"; icon = "sh:immich"; }
+                        { title = "Nginx Proxy Manager"; url = "http://192.168.122.130:81"; icon = "sh:nginx-proxy-manager"; }
+                      ];
+                    }
+                  ];
+                }
+                {
+                  type = "hacker-news";
+                  limit = 10;
+                  collapse-after = 5;
+                }
+              ];
+            }
+            {
+              size = "small";
+              widgets = [
+                { type = "server-stats"; }
+              ];
+            }
+          ];
+        }
+      ];
+    };
   };
 
-  # Open the port in firewall
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.allowedTCPPorts = [ 2283 80 443 81 8080 ];
 }
