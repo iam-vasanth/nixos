3227cccc4a8165ba7e6d28528d81bacd933cbf96
iam-vasanth <vk.vasanth.r@gmail.com>
2026-07-20 22:54:11 +0530

    Minor change

--- modules/home/programs/zathura.nix
+++ modules/home/programs/zathura.nix
@@ -4,22 +4,23 @@
   ###########################################################################
 
   programs.zathura = {
-  enable = true;
+    enable = true;
 
-  options = {
-    # Appearance
-    font = "JetBrainsMono Nerd Font 11";
-    recolor-lightcolor = "#1e1e2e";
-    recolor-darkcolor = "#cdd6f4";
-    default-bg = "#1e1e2e";
+    options = {
+      # Appearance
+      font = "JetBrainsMono Nerd Font 11";
+      recolor-lightcolor = "#1e1e2e";
+      recolor-darkcolor = "#cdd6f4";
+      default-bg = "#1e1e2e";
 
-    # Behavior
-    scroll-step = 50;
-    selection-clipboard = "clipboard";
-    window-title-basename = true;
-  };
+      # Behavior
+      scroll-step = 50;
+      selection-clipboard = "clipboard";
+      window-title-basename = true;
+    };
 
-  mappings = {
-    "<C-i>" = "recolor";
+    mappings = {
+      "<C-i>" = "recolor";
+    };
   };
 }
